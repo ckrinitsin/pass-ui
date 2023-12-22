96f127cce5e29e514f373498e9a0ed5353ca350e
Christian Krinitsin <christian.krinitsin@tum.de>
2023-12-22 21:31:29 +0100

    delete comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,3 @@
 int main() {
     return init();
 }
-
-
-// #!/usr/bin/expect -f
-// cd ~/uni/sem3/era/era-tutorium-ws2324/
-// spawn git pull
-// expect "ser"
-// send "ge87liq\r"
-// expect "ass"
-// send "Pokemo1n..\r"
-// interact
